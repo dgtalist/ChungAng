348eb1966307ceb5f94350cc969d4aebef5b7a3e
dgtalist <31330881+dgtalist@users.noreply.github.com>
2020-08-23 15:47:58 +0900

    Update 01_DHT11.py

--- Day_1/01_DHT11.py
+++ Day_1/01_DHT11.py
@@ -9,9 +9,9 @@ while True:
           
           
 [ 사전 설치 모듈 ]
-sudo apt-get update 
-sudo apt-get install python3-pip 
-sudo python3 -m pip install --upgrade pip setuptools wheel
-sudo pip3 install Adafruit_DHT
+$ sudo apt-get update 
+$ sudo apt-get install python3-pip 
+$ sudo python3 -m pip install --upgrade pip setuptools wheel
+$ sudo pip3 install Adafruit_DHT
 cd Adafruit_Python_DHT
 ./Adafruit_Python_DHT/$ sudo python3 setup.py install
