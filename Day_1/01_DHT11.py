00303f2a49802282a3a4606ddf624607c320eb62
dgtalist <31330881+dgtalist@users.noreply.github.com>
2020-08-23 15:47:37 +0900

    Update 01_DHT11.py

--- Day_1/01_DHT11.py
+++ Day_1/01_DHT11.py
@@ -5,3 +5,13 @@ while True:
     h,t = dht.read_retry(dht.DHT11,4)
     print('Humidity', h, '%', ', temperature', t, 'C’)
     time.sleep(1)
+
+          
+          
+[ 사전 설치 모듈 ]
+sudo apt-get update 
+sudo apt-get install python3-pip 
+sudo python3 -m pip install --upgrade pip setuptools wheel
+sudo pip3 install Adafruit_DHT
+cd Adafruit_Python_DHT
+./Adafruit_Python_DHT/$ sudo python3 setup.py install
